6d211ceaa1b5a79f5a080397cbed5804050259a7
uvsq21918311 <elkadi96hotmail.com>
2020-04-04 16:27:08 +0200

    Update class Serialization

--- src/main/java/fr/uvsq/pglp_5_1/Serialization.java
+++ src/main/java/fr/uvsq/pglp_5_1/Serialization.java
@@ -1,7 +1,7 @@
 package fr.uvsq.pglp_5_1;
 
 import fr.uvsq.pglp_5_1.AbstractDAOFactory.DAOType;
-
+import fr.uvsq.pglp_5_1.Personnel.PersonnelBuilder;;
 public enum Serialization {
 	ENVIRONNEMENT;
 	
@@ -13,22 +13,22 @@ public enum Serialization {
 				.getFactory(dt)
 				.getPersonnelGroupeDAO();
 		// Creation personnel
-		Personnel dupond = new PersonnelBuilder("Dupond", "Dupont", "Employé")
+		Personnel ELKADI = new PersonnelBuilder("EL KADI", "Kamal", "Employé")
 				   			.build();
-		Personnel dupont = new PersonnelBuilder("Dupont", "Dupond", "Employé")
+		Personnel Kamal = new PersonnelBuilder("EL KADI", "Kamal", "Employé")
 							.build();
 		PersonnelGroupe pg = new PersonnelGroupe();
 		PersonnelGroupe spg = new PersonnelGroupe();
 		pg.addPersonnel(spg);
-		pg.addPersonnel(dupont);
-		spg.addPersonnel(dupond);
+		pg.addPersonnel(Kamal);
+		spg.addPersonnel(ELKADI);
 		// Ajout DAO
 		pgDAO.create(pg);
 		pgDAO.create(spg);
-		personnelDAO.create(dupond); // update
-		personnelDAO.create(dupont); // update
+		personnelDAO.create(ELKADI); // update
+		personnelDAO.create(Kamal); // update
 		System.out.println("\t" + pgDAO.read(spg.getId()));
-		System.out.println("\t" + personnelDAO.read("Dupont"));
+		System.out.println("\t" + personnelDAO.read("Kamal"));
 	}
 	
 	public static void main(String[] args) {
